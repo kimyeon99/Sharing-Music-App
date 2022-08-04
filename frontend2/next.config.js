cfaed3e38cd7710791116b2fcbe8fcbdf6ca5572
kimyeon99 <tmdwo7017@naver.com>
2022-08-04 20:25:50 +0900

    Add - ServerSideRendering

--- frontend2/next.config.js
+++ frontend2/next.config.js
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
+//key 隠し
+const API_KEY = "711ff0b4ef9e9ee1377ed51511bd5560";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // 주소 바뀌는 redirect
+  async redirects(){
+    return [
+      {
+        source:"/contact/:path*",
+        destination:"/form/:path*",
+        permanent:false,
+      }
+    ]
+  },
+  // 주소 안 바뀌는 redirect
+  // masking
+  async rewrites(){
+    return [{
+      source:"/api/movies",
+      destination:`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+    }]
+  }
 }
 
 module.exports = nextConfig
--- frontend2/pages/index.js
+++ frontend2/pages/index.js
@@ -1,29 +1,49 @@
 import { useEffect, useState } from "react";
 import Title from "../components/Title";
 
-const API_KEY = "711ff0b4ef9e9ee1377ed51511bd5560";
-
-export default function Home(){
+export default function Home({results}){
     const [moives, setMovies] = useState();
     
-    useEffect(() => {
-        (async() => {
-            const {results} = await (
-                await fetch(
-                `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-            )).json();
-            setMovies(results);
-            console.log(results);
-        })();
-    }, []);
     return (
-        <div>
-            <Title title="Home"/>
-            {!moives && <h4>Loading...</h4>}
-            {moives?.map((movie) => 
-                <div key={movie.id}>
+        <div className="container">
+            <Title title="Home"/>            
+            {results?.map((movie) => 
+                <div className={movie} key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                     <h4>{movie.original_title}</h4>
                 </div>)}
+        <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
         </div>
+        
     )
+}
+// getServerSideProps = 고정
+// ㄴ 서버 사이드 렌더링
+export async function getServerSideProps(){
+  const {results} = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results, 
+    }
+  }
 }
