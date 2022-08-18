b2a1d504cb9b140b7fd2a76a24e4501cd6754825
kimyeon99 <tmdwo7017@naver.com>
2022-08-18 16:32:12 +0900

    pathch

--- frontend2/pages/index.js
+++ frontend2/pages/index.js
@@ -1,17 +1,39 @@
-import { useEffect, useState } from "react";
 import Title from "../components/Title";
+import Link from 'next/link';
+import { useRouter } from 'next/router'
+
 
 export default function Home({results}){
-    const [moives, setMovies] = useState();
+    const router = useRouter();
+    const onClick = (id, title) => {
+      router.push(
+        {
+          pathname:`/movies/${id}`,
+          query:{
+            title
+          },
+      }, `/movies/${id}`);
+    }
     
     return (
         <div className="container">
             <Title title="Home"/>            
             {results?.map((movie) => 
-                <div className={movie} key={movie.id}>
+                  <div className="movie" key={movie.id} onClick={()=>onClick(movie.id, movie.original_title)}>
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                    <h4>{movie.original_title}</h4>
-                </div>)}
+                    <h4>
+                      <Link href={{
+                        pathname:`/movies/${movie.id}`,
+                        query:{
+                          title:movie.original_title,
+                        }
+                      }}
+                      as={`/movies/${movie.id}`} key={movie.id}>
+                        <a>{movie.original_title}</a>
+                      </Link>
+                    </h4>
+                  </div>
+                )}
         <style jsx>{`
         .container {
           display: grid;
